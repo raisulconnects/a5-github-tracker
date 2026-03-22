1af79e858510fa38b84bf327063d2e2d5477391b
Raisul <raisulconnects@gmail.com>
2026-03-22 13:14:55 +0600

    Search Functionality working

--- dashboard-script.js
+++ dashboard-script.js
@@ -13,6 +13,9 @@ const allBtn = document.getElementById("allBtn");
 const openBtn = document.getElementById("openBtn");
 const closedBtn = document.getElementById("closedBtn");
 
+const searchInput = document.getElementById("searchInput");
+const searchBtn = document.getElementById("searchBtn");
+
 let content = "";
 
 // All Tab Fetch Function
@@ -209,6 +212,71 @@ const githubFetcherClosed = async () => {
   }
 };
 
+// Search Fetch Function
+const githubFetcherSearch = async (searchText) => {
+  try {
+    const response = await fetch(
+      `https://phi-lab-server.vercel.app/api/v1/lab/issues/search?q=${searchText}`,
+    );
+    const data = await response.json();
+
+    content = data.data
+      .map((issue) => {
+        return `
+
+          <div class="card border-t-4 border-${issue.status === "open" ? "green" : "purple"}-500">
+            <div
+              class="w-full max-w-sm border border-gray-200 rounded-xl p-4 bg-white shadow-sm flex flex-col h-full min-h-[260px]"
+            >
+              <!-- Top Section -->
+              <div class="flex items-start justify-between mb-3">
+                <img src="./images/${issue.status}-Status.png" alt="" />
+
+                <span class="text-xs font-semibold px-3 py-1 rounded-full bg-red-100 text-red-500">
+                  HIGH
+                </span>
+              </div>
+
+              <h2 class="text-lg font-semibold text-gray-800">
+                ${issue.title}
+              </h2>
+
+              <p class="text-sm text-gray-500 mt-2">
+                ${issue.description}
+              </p>
+
+              <!-- Tags -->
+              <div class="flex gap-2 mt-4 mb-2">
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-red-300 text-red-500 bg-red-50">
+                  <img src="./images/BugDroid.png" alt="" /> BUG
+                </span>
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50 whitespace-nowrap">
+                  <img src="./images/Lifebuoy.png" alt="" /> HELP WANTED
+                </span>
+              </div>
+
+              <!-- Footer (pushed to bottom) -->
+              <div class="mt-auto pt-2 border-t text-xs text-gray-400">
+                <p>#${issue.id} by ${issue.author}</p>
+                <p class="pt-1">
+                  ${new Date(issue.createdAt).toLocaleDateString()}
+                </p>
+              </div>
+            </div>
+          </div>
+
+`;
+      })
+      .join("");
+
+    cardBox.innerHTML = "";
+    cardBox.innerHTML = content;
+    issuesNumber.textContent = `${data.data.length} Issues`;
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
 cardBox.addEventListener("click", function (e) {
   console.log("lol");
 });
@@ -216,8 +284,11 @@ cardBox.addEventListener("click", function (e) {
 // Initially All Tab will be active and show all the issues
 githubFetcher();
 
-// Add Event Listeners to the buttons
+// Add Event Listeners to the buttons & Loading Spinner while fetching data
 allBtn.addEventListener("click", function () {
+  cardBox.innerHTML =
+    '<div class="flex justify-center items-center"> <span class="loading loading-spinner text-error"></span> </div>';
+
   githubFetcher();
 
   allBtn.classList.add("bg-[#4A00FF]", "text-white");
@@ -233,6 +304,9 @@ allBtn.addEventListener("click", function () {
 });
 
 openBtn.addEventListener("click", function () {
+  cardBox.innerHTML =
+    '<div class="flex justify-center items-center"> <span class="loading loading-spinner text-error"></span> </div>';
+
   githubFetcherOpen();
 
   openBtn.classList.add("bg-[#4A00FF]", "text-white");
@@ -248,6 +322,9 @@ openBtn.addEventListener("click", function () {
 });
 
 closedBtn.addEventListener("click", function () {
+  cardBox.innerHTML =
+    '<div class="flex justify-center items-center"> <span class="loading loading-spinner text-error"></span> </div>';
+
   githubFetcherClosed();
 
   closedBtn.classList.add("bg-[#4A00FF]", "text-white");
@@ -260,4 +337,13 @@ closedBtn.addEventListener("click", function () {
   openBtn.classList.add("bg-white");
 
   console.log("Closed Github Fetcher Clicked!");
+});
+
+searchBtn.addEventListener("click", function () {
+  cardBox.innerHTML =
+    '<div class="flex justify-center items-center"> <span class="loading loading-spinner text-error"></span> </div>';
+
+  const searchText = searchInput.value.trim();
+  githubFetcherSearch(searchText);
+  console.log("Search Github Fetcher Clicked!");
 });
