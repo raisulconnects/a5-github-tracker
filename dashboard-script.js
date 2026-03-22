81701158dce207ae78a4ff6cd2d7d1f7ce434f85
Raisul <raisulconnects@gmail.com>
2026-03-22 12:21:59 +0600

    All the tabs are loading properly now

--- dashboard-script.js
+++ dashboard-script.js
@@ -9,8 +9,13 @@ console.log("---------------------------------------------");
 const cardBox = document.getElementById("card-box");
 const issuesNumber = document.getElementById("issuesNumber");
 
+const allBtn = document.getElementById("allBtn");
+const openBtn = document.getElementById("openBtn");
+const closedBtn = document.getElementById("closedBtn");
+
 let content = "";
 
+// All Tab Fetch Function
 const githubFetcher = async () => {
   try {
     const response = await fetch(
@@ -20,63 +25,191 @@ const githubFetcher = async () => {
     content = data.data
       .map((issue) => {
         return `
-        
-        <div class="card">
-          <div
-            class="w-full max-w-sm border border-gray-200 rounded-xl p-4 bg-white shadow-sm"
-          >
-            <!-- Top Section -->
-            <div class="flex items-start justify-between mb-3">
-              <!-- Icon -->
-              <img src="./images/${issue.status}-Status.png" alt="" />
 
-              <!-- Priority Badge -->
-              <span
-                class="text-xs font-semibold px-3 py-1 rounded-full bg-red-100 text-red-500"
-              >
-                HIGH
-              </span>
-            </div>
+          <div class="card border-t-4 border-${issue.status === "open" ? "green" : "purple"}-500">
+            <div
+              class="w-full max-w-sm border border-gray-200 rounded-xl p-4 bg-white shadow-sm flex flex-col h-full min-h-[260px]"
+            >
+              <!-- Top Section -->
+              <div class="flex items-start justify-between mb-3">
+                <img src="./images/${issue.status}-Status.png" alt="" />
 
-            <!-- Title -->
-            <h2 class="text-lg font-semibold text-gray-800 leading-snug">
-              ${issue.title}
-            </h2>
+                <span class="text-xs font-semibold px-3 py-1 rounded-full bg-red-100 text-red-500">
+                  HIGH
+                </span>
+              </div>
 
-            <!-- Description -->
-            <p class="text-sm text-gray-500 mt-2">
-              ${issue.description}
-            </p>
+              <h2 class="text-lg font-semibold text-gray-800">
+                ${issue.title}
+              </h2>
 
-            <!-- Tags -->
-            <div class="flex gap-2 mt-4">
-              <span
-                class="flex gap-1 text-xs px-3 py-1 rounded-full border border-red-300 text-red-500 bg-red-50"
-              >
-                <img src="./images/BugDroid.png" alt="" /> BUG
-              </span>
-              <span
-                class="flex gap-1 text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50 text-nowrap"
-              >
-                <img src="./images/Lifebuoy.png" alt="" /> HELP WANTED
-              </span>
-            </div>
+              <p class="text-sm text-gray-500 mt-2">
+                ${issue.description}
+              </p>
 
-            <!-- Footer -->
-            <div class="mt-4 pt-3 border-t text-xs text-gray-400">
-              <p> #${issue.id} by ${issue.author}</p>
-              <p class="pt-1">${new Date(issue.createdAt).toLocaleString().split(",")[0]}</p>
+              <!-- Tags -->
+              <div class="flex gap-2 mt-4 mb-2">
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-red-300 text-red-500 bg-red-50">
+                  <img src="./images/BugDroid.png" alt="" /> BUG
+                </span>
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50 whitespace-nowrap">
+                  <img src="./images/Lifebuoy.png" alt="" /> HELP WANTED
+                </span>
+              </div>
+
+              <!-- Footer (pushed to bottom) -->
+              <div class="mt-auto pt-2 border-t text-xs text-gray-400">
+                <p>#${issue.id} by ${issue.author}</p>
+                <p class="pt-1">
+                  ${new Date(issue.createdAt).toLocaleDateString()}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-        `;
+
+`;
       })
       .join("");
 
+    cardBox.innerHTML = "";
     cardBox.innerHTML = content;
     issuesNumber.textContent = `${data.data.length} Issues`;
 
-    console.log(data.data);
+    console.log("All Github Fetcher Clicked!");
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+// Open Tab Fetch Function
+const githubFetcherOpen = async () => {
+  try {
+    const response = await fetch(
+      "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+    );
+    const data = await response.json();
+    content = data.data
+      .filter((issue) => issue.status === "open")
+      .map((issue) => {
+        return `
+
+          <div class="card border-t-4 border-${issue.status === "open" ? "green" : "purple"}-500">
+            <div
+              class="w-full max-w-sm border border-gray-200 rounded-xl p-4 bg-white shadow-sm flex flex-col h-full min-h-[260px]"
+            >
+              <!-- Top Section -->
+              <div class="flex items-start justify-between mb-3">
+                <img src="./images/${issue.status}-Status.png" alt="" />
+
+                <span class="text-xs font-semibold px-3 py-1 rounded-full bg-red-100 text-red-500">
+                  HIGH
+                </span>
+              </div>
+
+              <h2 class="text-lg font-semibold text-gray-800">
+                ${issue.title}
+              </h2>
+
+              <p class="text-sm text-gray-500 mt-2">
+                ${issue.description}
+              </p>
+
+              <!-- Tags -->
+              <div class="flex gap-2 mt-4 mb-2">
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-red-300 text-red-500 bg-red-50">
+                  <img src="./images/BugDroid.png" alt="" /> BUG
+                </span>
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50 whitespace-nowrap">
+                  <img src="./images/Lifebuoy.png" alt="" /> HELP WANTED
+                </span>
+              </div>
+
+              <!-- Footer (pushed to bottom) -->
+              <div class="mt-auto pt-2 border-t text-xs text-gray-400">
+                <p>#${issue.id} by ${issue.author}</p>
+                <p class="pt-1">
+                  ${new Date(issue.createdAt).toLocaleDateString()}
+                </p>
+              </div>
+            </div>
+          </div>
+
+`;
+      })
+      .join("");
+
+    cardBox.innerHTML = "";
+    cardBox.innerHTML = content;
+    issuesNumber.textContent = `${data.data.filter((issue) => issue.status === "open").length} Issues`;
+
+    console.log("Open Github Fetcher Clicked!");
+  } catch (e) {
+    console.log(e.message);
+  }
+};
+
+// Close Tab Fetch Function
+const githubFetcherClosed = async () => {
+  try {
+    const response = await fetch(
+      "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+    );
+    const data = await response.json();
+    content = data.data
+      .filter((issue) => issue.status === "closed")
+      .map((issue) => {
+        return `
+
+          <div class="card border-t-4 border-${issue.status === "open" ? "green" : "purple"}-500">
+            <div
+              class="w-full max-w-sm border border-gray-200 rounded-xl p-4 bg-white shadow-sm flex flex-col h-full min-h-[260px]"
+            >
+              <!-- Top Section -->
+              <div class="flex items-start justify-between mb-3">
+                <img src="./images/${issue.status}-Status.png" alt="" />
+
+                <span class="text-xs font-semibold px-3 py-1 rounded-full bg-red-100 text-red-500">
+                  HIGH
+                </span>
+              </div>
+
+              <h2 class="text-lg font-semibold text-gray-800">
+                ${issue.title}
+              </h2>
+
+              <p class="text-sm text-gray-500 mt-2">
+                ${issue.description}
+              </p>
+
+              <!-- Tags -->
+              <div class="flex gap-2 mt-4 mb-2">
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-red-300 text-red-500 bg-red-50">
+                  <img src="./images/BugDroid.png" alt="" /> BUG
+                </span>
+                <span class="flex gap-1 text-xs px-3 py-1 rounded-full border border-yellow-300 text-yellow-600 bg-yellow-50 whitespace-nowrap">
+                  <img src="./images/Lifebuoy.png" alt="" /> HELP WANTED
+                </span>
+              </div>
+
+              <!-- Footer (pushed to bottom) -->
+              <div class="mt-auto pt-2 border-t text-xs text-gray-400">
+                <p>#${issue.id} by ${issue.author}</p>
+                <p class="pt-1">
+                  ${new Date(issue.createdAt).toLocaleDateString()}
+                </p>
+              </div>
+            </div>
+          </div>
+
+`;
+      })
+      .join("");
+
+    cardBox.innerHTML = "";
+    cardBox.innerHTML = content;
+    issuesNumber.textContent = `${data.data.filter((issue) => issue.status === "closed").length} Issues`;
+
+    console.log("Closed Github Fetcher Clicked!");
   } catch (e) {
     console.log(e.message);
   }
@@ -87,3 +220,8 @@ cardBox.addEventListener("click", function (e) {
 });
 
 githubFetcher();
+
+// Add Event Listeners to the buttons
+allBtn.addEventListener("click", githubFetcher);
+openBtn.addEventListener("click", githubFetcherOpen);
+closedBtn.addEventListener("click", githubFetcherClosed);
